2b02f7545cc9f97d1dfe4463bcb10e6ccf2b1c7c
Ville Suokas <vilsuo1@gmail.com>
2023-10-04 01:35:08 +0300

    completed part 5.16

--- part5/bloglist-frontend/src/components/BlogForm.test.js
+++ part5/bloglist-frontend/src/components/BlogForm.test.js
@@ -4,20 +4,40 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import BlogForm from './BlogForm'
 
-describe('', () => {
-  let container
-
-  const addBlog = jest.fn()
+describe('filling all inputs with valid data', () => {
+  let user
+  const addBlog = jest.fn(x => true)
 
   beforeEach(() => {
-    container = render(
+    user = userEvent.setup()
+
+    render(
       <BlogForm
         addBlog={addBlog}
       />
     )
   })
 
-  test('', () => {
+  test('form submits input values to blog adding handler', async () => {
+    // select inputs...
+    const titleInput = screen.getByPlaceholderText('give title')
+    const authorInput = screen.getByPlaceholderText('give author')
+    const urlInput = screen.getByPlaceholderText('give url')
 
+    // and insert values..
+    await user.type(titleInput, 'Awesome blog')
+    await user.type(authorInput, 'Me')
+    await user.type(urlInput, 'read-it-here')
+
+    // click submit
+    const submitButton = screen.getByText('create')
+    await user.click(submitButton)
+
+    expect(addBlog.mock.calls).toHaveLength(1)
+    expect(addBlog.mock.calls[0][0]).toEqual({
+      title: 'Awesome blog',
+      author: 'Me',
+      url: 'read-it-here'
+    })
   })
 })
