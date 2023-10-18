90f9e285ff33669c9a81fa7d12296e17dc65e4d2
Ville Suokas <vilsuo1@gmail.com>
2023-10-19 01:42:28 +0300

    completed part 13.4

--- part13/blogsbackendrelational/index.js
+++ part13/blogsbackendrelational/index.js
@@ -1,39 +1,78 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize')
 
-/*
 const express = require('express')
 
 const app = express()
 app.use(express.json())
-*/
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-  const blogs = await sequelize.query(
-    'SELECT * FROM blogs',
-    { type: QueryTypes.SELECT }
-  )
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
 
-  blogs.forEach(blog => {
-    console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-  })
-}
-
-main();
-/*
 app.get('/api/blogs', async (req, res) => {
-  const blogs = await sequelize.query(
-    'SELECT * FROM blogs',
-    { type: QueryTypes.SELECT }
-  )
-
+  const blogs = await Blog.findAll()
   res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const body = req.body;
+    const newBlog = await Blog.create(body)
+
+    res.send(newBlog)
+  } catch (error) {
+    res.status(400).end()
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const id = req.params.id
+  await Blog.destroy({ where: { id }})
+  /*
+  const blogToDelete = await Blog.findByPk(id)
+  if (blogToDelete) {
+    await blogToDelete.destroy();
+  }
+  */
+  res.status(204).end()
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port=${PORT}`)
 })
-*/
