5ee200b2f78df2a438879c4c85b7dca2a5e9ea63
Ville Suokas <vilsuo1@gmail.com>
2023-10-31 16:28:16 +0200

    completed part 12.9

--- part12-containers-applications/todo-app/todo-backend/redis/index.js
+++ part12-containers-applications/todo-app/todo-backend/redis/index.js
@@ -10,15 +10,16 @@ if (!REDIS_URL) {
     console.log('No REDIS_URL set, Redis is disabled')
     return null
   }
+
   getAsync = redisIsDisabled
   setAsync = redisIsDisabled
 } else {
   const client = redis.createClient({
     url: REDIS_URL
   })
-    
+
   getAsync = promisify(client.get).bind(client)
-  setAsync = promisify(client.set).bind(client)    
+  setAsync = promisify(client.set).bind(client)
 }
 
 module.exports = {
--- part12-containers-applications/todo-app/todo-backend/routes/index.js
+++ part12-containers-applications/todo-app/todo-backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const redis = require('../redis');
+
 const configs = require('../util/config')
 
 let visits = 0
